ad5e9993d08d6bc87631dfccf59be1f578afec7c
Yuri Voskanyan <yura.voskanyan.2003@mail.ru>
2023-04-27 10:24:12 +0700

    Добавлена текст сохранении пустого поля

--- task_manager/app/src/main/java/com/example/task_manager/add_activity.java
+++ task_manager/app/src/main/java/com/example/task_manager/add_activity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.task_manager.db.MyDbManager;
@@ -14,20 +15,21 @@ public class add_activity extends AppCompatActivity {
 
     Toast toast;
     EditText entertext;
+    TextView text;
     private MyDbManager myDbManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
         entertext = findViewById(R.id.enter_text);
+        text = findViewById(R.id.error_text);
         myDbManager = new MyDbManager(this);
         myDbManager.openDb();
     }
     public void save(View view){
         //entertext.getText();
         if(entertext.getText().toString().equals("")){
-            String empty_line = "Вы пытаетесь добавить пустую задачу. Не считаете ли вы это странным?\uD83D\uDE10";
-            toast = Toast.makeText(getApplicationContext(), empty_line, Toast.LENGTH_LONG);toast.show();
+            text.setText("Поле не должно быть пустым!");
         }
         else{
             myDbManager.insertToDb(entertext.getText().toString());
